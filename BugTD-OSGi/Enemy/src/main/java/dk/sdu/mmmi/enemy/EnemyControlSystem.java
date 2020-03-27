c9d1ff25e3b425de3dee50719e0eb43c27f4670e
Thomas Steenfeldt Laursen <tlaur18@student.sdu.dk>
2020-03-27 10:59:30 +0100

    Resolved compile errors

--- BugTD-OSGi/Enemy/src/main/java/dk/sdu/mmmi/enemy/EnemyControlSystem.java
+++ BugTD-OSGi/Enemy/src/main/java/dk/sdu/mmmi/enemy/EnemyControlSystem.java
@@ -31,37 +31,11 @@ public class EnemyControlSystem implements IEntityProcessingService {
             
             movingPart.process(gameData,enemy);
             p.process(gameData,enemy);
-
-            updateShape(enemy);
             
             if(((LifePart) enemy.getPart(LifePart.class)).isDead()) {
                 world.removeEntity(enemy);
             }
         }
-    }
-    
-    private void updateShape(Entity entity) {
-        float[] shapex = entity.getShapeX();
-        float[] shapey = entity.getShapeY();
-        PositionPart positionPart = entity.getPart(PositionPart.class);
-        float x = positionPart.getX();
-        float y = positionPart.getY();
-        float radians = positionPart.getRadians();
-
-        shapex[0] = (float) (x + Math.cos(radians) * 8);
-        shapey[0] = (float) (y + Math.sin(radians) * 8);
-
-        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * 8);
-        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * 8);
-
-        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * 5);
-        shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * 5);
-
-        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * 8);
-        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * 8);
-
-        entity.setShapeX(shapex);
-        entity.setShapeY(shapey);
     }
     
 }
