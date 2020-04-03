b3226883e864c8ff51752508b527a2168d7d7c26
tlaur18 <tlaur18@student.sdu.dk>
2020-04-03 10:40:27 +0200

    Removed enemies

--- BugTD-OSGi/Enemy/src/main/java/dk/sdu/mmmi/enemy/FlyingEnemy.java
+++ BugTD-OSGi/Enemy/src/main/java/dk/sdu/mmmi/enemy/FlyingEnemy.java
@@ -3,11 +3,6 @@ package dk.sdu.mmmi.enemy;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
-import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
-import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
-import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
-import dk.sdu.mmmi.cbse.common.data.entityparts.SpritePart;
-import dk.sdu.mmmi.cbse.common.data.entityparts.WeaponPart;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 import dk.sdu.mmmi.commonenemy.Enemy;
 
@@ -19,33 +14,6 @@ public class FlyingEnemy implements IGamePluginService {
 
     @Override
     public void start(GameData gameData, World world) {
-        Entity fEnemy = new Enemy();
-        //attributes
-        float damage = 10;
-        float range = 50;
-        float speed = 10;
-        float deacceleration = 10;
-        float acceleration = 200;
-        float maxSpeed = 300;
-        float rotationSpeed = 5;
-        float x = gameData.getDisplayWidth() / 3;
-        float y = gameData.getDisplayHeight() / 2;
-        float radians = 0;
-        int life = 100;
-        
-
-        //Parts
-        SpritePart sprite = new SpritePart("anty-boi-in-red.png", 32, 32);
-        fEnemy.add(sprite);
-        WeaponPart wpn = new WeaponPart(damage, range, speed);
-        fEnemy.add(wpn);
-        MovingPart mov = new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed);
-        fEnemy.add(mov);
-        PositionPart pos  = new PositionPart(x, y, radians);
-        fEnemy.add(pos);
-        LifePart lif = new LifePart(life);
-        fEnemy.add(lif);
-        world.addEntity(fEnemy);
     }
 
     @Override
@@ -54,5 +22,5 @@ public class FlyingEnemy implements IGamePluginService {
             world.removeEntity(enemy);
         }
     }
-    
+
 }
