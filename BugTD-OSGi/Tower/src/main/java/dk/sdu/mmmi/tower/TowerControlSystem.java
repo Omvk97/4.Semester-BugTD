c6070bccc6ae48c685e037f70175ddba9e517d03
tlaur18 <tlaur18@student.sdu.dk>
2020-04-03 10:47:59 +0200

    Merge corrections

--- BugTD-OSGi/Tower/src/main/java/dk/sdu/mmmi/tower/TowerControlSystem.java
+++ BugTD-OSGi/Tower/src/main/java/dk/sdu/mmmi/tower/TowerControlSystem.java
@@ -111,7 +111,7 @@ public class TowerControlSystem implements IEntityProcessingService {
         int hp = 100;
         LifePart life = new LifePart(hp);
 
-        CollisionPart colli = new CollisionPart();
+        CollisionPart colli = new CollisionPart(32, 32);
 
         float damage = 10;
         float range = 50;
