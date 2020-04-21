f699f68dcd52e41293813858cd32a0a73dfca29b
Morten K. Jensen <mortj18@student.sdu.dk>
2020-04-21 12:06:20 +0200

    merge

--- BugTD-OSGi/OSGIEnemySpawner/src/main/java/dk/sdu/mmmi/osgienemyspawner/EnemySpawner.java
+++ BugTD-OSGi/OSGIEnemySpawner/src/main/java/dk/sdu/mmmi/osgienemyspawner/EnemySpawner.java
@@ -1,24 +1,16 @@
 package dk.sdu.mmmi.osgienemyspawner;
 
-import dk.sdu.mmmi.cbse.common.data.Entity;
+
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
-import dk.sdu.mmmi.cbse.common.data.entityparts.AnimationPart;
-import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
-import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
-import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
-import dk.sdu.mmmi.cbse.common.data.entityparts.WeaponPart;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
-import dk.sdu.mmmi.commonenemy.Enemy;
 import dk.sdu.mmmi.enemy.GroundEnemy;
-import java.util.Timer;
-import javafx.concurrent.Task;
 
 public class EnemySpawner implements IEntityProcessingService {
 
     private int totalEnemysInWorld = 0;
     private int targetEnemyCount = 5;
-    private float timeDelay = 30f;
+    private float timeDelay = 60f;
     private float lastSpawn = 0f;
 
     @Override
@@ -33,7 +25,5 @@ public class EnemySpawner implements IEntityProcessingService {
             }
             lastSpawn = lastSpawn + 0.5f;
         }
-
     }
-
 }
