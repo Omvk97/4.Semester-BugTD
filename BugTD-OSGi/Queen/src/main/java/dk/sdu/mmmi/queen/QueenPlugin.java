e602c9462971edac4d74652d2eea766c961a143b
Nicolai Gram <nicolaigram@gmail.com>
2020-04-25 12:35:14 +0200

    Rework queen module

--- BugTD-OSGi/Queen/src/main/java/dk/sdu/mmmi/queen/QueenPlugin.java
+++ BugTD-OSGi/Queen/src/main/java/dk/sdu/mmmi/queen/QueenPlugin.java
@@ -14,17 +14,8 @@ import dk.sdu.mmmi.commontower.Queen;
 
 public class QueenPlugin implements IGamePluginService {
     
-    public final static String QUEEN_PATH = "towers/queen.png";
-
     @Override
     public void start(GameData gameData, World world) {
-        PositionPart pos = new PositionPart(gameData.getDisplayWidth() / 2 - 2*TileSizes.GRASS_WIDTH, gameData.getDisplayHeight() / 10, 0);
-        LifePart life = new LifePart(100);
-        CollisionPart collision = new CollisionPart(4*TileSizes.GRASS_WIDTH, 4*TileSizes.GRASS_HEIGHT);
-        WeaponPart weapon = new WeaponPart(10, 10, 1);
-        SpritePart sprite = new SpritePart(QUEEN_PATH, 4*TileSizes.GRASS_WIDTH, 4*TileSizes.GRASS_HEIGHT, 1);
-        Entity queen = new Queen(pos, life, collision, weapon, sprite);
-        world.addEntity(queen);
     }
 
     @Override
