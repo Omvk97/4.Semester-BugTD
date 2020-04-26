f77b16bd16e50d7407058324aa05c3ee767ea3eb
tlaur18 <tlaur18@student.sdu.dk>
2020-04-26 10:17:14 +0200

    CommonEnemy updated

--- BugTD-OSGi/CommonEnemy/src/main/java/dk/sdu/mmmi/commonenemy/Enemy.java
+++ BugTD-OSGi/CommonEnemy/src/main/java/dk/sdu/mmmi/commonenemy/Enemy.java
@@ -5,9 +5,22 @@ import dk.sdu.mmmi.cbse.common.data.entityparts.AnimationPart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PreciseMovementInstruction;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PreciseMovingPart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.WeaponPart;
+import dk.sdu.mmmi.commonai.events.EnemyCommand;
+import java.util.List;
 
 public class Enemy extends Entity {
+
+    private List<EnemyCommand> commands = null;
+    private int commandIndex = 0;
+    private EnemyType type;
+    private WeaponPart weaponPart;
+    private AnimationPart animationPart;
+    private PositionPart positionPart;
+    private PreciseMovingPart movingPart;
+    private LifePart lifePart;
 
     public static Entity createGroundEnemy(float x, float y) {
         Entity gEnemy = new Enemy();
@@ -71,4 +84,94 @@ public class Enemy extends Entity {
         return fEnemy;
     }
 
+    public Enemy() {
+        super();
+    }
+
+    public Enemy(EnemyType type, WeaponPart weaponPart, AnimationPart animationPart, PositionPart positionPart, PreciseMovingPart movingPart, LifePart lifePart) {
+        this.type = type;
+        this.weaponPart = weaponPart;
+        add(weaponPart);
+        this.animationPart = animationPart;
+        add(animationPart);
+        this.positionPart = positionPart;
+        add(positionPart);
+        this.movingPart = movingPart;
+        add(movingPart);
+        this.lifePart = lifePart;
+        add(lifePart);
+    }
+
+    public List<EnemyCommand> getCommands() {
+        return commands;
+    }
+
+    public void setCommands(List<EnemyCommand> moveCommands) {
+        this.commands = moveCommands;
+        resetCommandIndex();
+    }
+
+    public void incrementCommandIndex() {
+        this.commandIndex++;
+    }
+
+    private void resetCommandIndex() {
+        this.commandIndex = 0;
+    }
+
+    public int getCommandIndex() {
+        return this.commandIndex;
+    }
+
+    public boolean isDoneFollowingCommands() {
+        return commandIndex == commands.size() - 1;
+    }
+
+    public float getXTarget() {
+        return commands.get(commandIndex).getX();
+    }
+
+    public float getYTarget() {
+        return commands.get(commandIndex).getY();
+    }
+
+    public EnemyType getType() {
+        return type;
+    }
+
+    public float getCurrentX() {
+        return positionPart.getX();
+    }
+
+    public float getCurrentY() {
+        return positionPart.getY();
+    }
+
+    public float calculateFutureXPosition() {
+        return movingPart.calculateFutureXPosition(getCurrentX());
+    }
+
+    public float calculateFutureYPosition() {
+        return movingPart.calculateFutureYPosition(getCurrentY());
+    }
+
+    public void addMovement(PreciseMovementInstruction preciseMovementInstruction) {
+        movingPart.addMovement(preciseMovementInstruction);
+    }
+
+    public WeaponPart getWeaponPart() {
+        return weaponPart;
+    }
+
+    public AnimationPart getAnimationPart() {
+        return animationPart;
+    }
+
+    public PositionPart getPositionPart() {
+        return positionPart;
+    }
+
+    public PreciseMovingPart getMovingPart() {
+        return movingPart;
+    }
 }
