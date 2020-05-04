aa28831a3dee4cd33994a6d42328006e33346a9a
Nicolai Gram <nicolaigram@gmail.com>
2020-05-04 08:41:02 +0200

    Refactor into gamepluginservice and class, making it possible to unload

--- BugTD-OSGi/OSGIEnemySpawner/src/main/java/dk/sdu/mmmi/osgienemyspawner/EnemySpawner.java
+++ BugTD-OSGi/OSGIEnemySpawner/src/main/java/dk/sdu/mmmi/osgienemyspawner/EnemySpawner.java
@@ -1,24 +1,18 @@
 package dk.sdu.mmmi.osgienemyspawner;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.events.EnemyDiedEvent;
 import dk.sdu.mmmi.cbse.common.events.Event;
-import dk.sdu.mmmi.cbse.common.events.GameOverEvent;
 import dk.sdu.mmmi.cbse.common.events.GameWonEvent;
-import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
-import dk.sdu.mmmi.commonai.events.EnemySpawnedEvent;
 import dk.sdu.mmmi.commonenemy.Enemy;
 import dk.sdu.mmmi.commonmap.MapSPI;
 import dk.sdu.mmmi.commonmap.MapWave;
 
 import java.util.ArrayList;
 
-public class EnemySpawner implements IEntityProcessingService {
-
+public class EnemySpawner extends Entity {
     private int totalEnemysInWorld = 0;
     private int targetEnemyCount = 0;
     private int deadEnemies = 0;
@@ -30,39 +24,17 @@ public class EnemySpawner implements IEntityProcessingService {
     private int currentWaveNumber = 1;
     private String enemyType;
     private MapWave currentWave = null;
-    private MapSPI mapSPI;
     private ArrayList<MapWave> waves = new ArrayList<>();
 
-    @Override
-    public void process(GameData gameData, World world) {
-        if (waves.isEmpty()) {
-            waves = mapSPI.getMapWaves();
-        }
-        updateWaveInfo();
-        getEnemyAmountInWorld(world, gameData);
-        calculateCurrentRound(gameData);
-        lastSpawn = lastSpawn + gameData.getDelta();
-
-        if (enemiesLeftToSpawn > 0) {
-            if (lastSpawn > spawnTime) {
-                // System.out.println("Spawning enemy");
-                if ("Ground".equals(enemyType)) {
-                    Entity enemy = Enemy.createGroundEnemy(mapSPI.getEnemySpawnX(), mapSPI.getEnemySpawnY(), currentWave.getEnemyLife());
-                    world.addEntity(enemy);
-                    gameData.addEvent(new EnemySpawnedEvent(enemy));
-                } else if ("Flying".equals(enemyType)) {
-                    Entity enemy = Enemy.createFlyingEnemy(mapSPI.getEnemySpawnX(), mapSPI.getEnemySpawnY(), currentWave.getEnemyLife());
-                    world.addEntity(enemy);
-                    gameData.addEvent(new EnemySpawnedEvent(enemy));
-                }
-                
-                lastSpawn = 0f;
-            }
-        }
+    // Gets all info from the MapWave and updates the variables.
+    public void updateWaveInfo() {
+        currentWave = waves.get(currentWaveNumber - 1);
+        targetEnemyCount = currentWave.getEnemyAmount();
+        enemyType = currentWave.getEnemyType();
     }
 
     //Counts the amount of enemies in the world, and updates the total.
-    private void getEnemyAmountInWorld(World world, GameData gameData) {
+    public void getEnemyAmountInWorld(World world, GameData gameData) {
         int enemiesInWorld = 0;
         if (!gameData.getEvents(EnemyDiedEvent.class).isEmpty()) {
             for (Event enemyDiedEvent : gameData.getEvents(EnemyDiedEvent.class)) {
@@ -77,22 +49,6 @@ public class EnemySpawner implements IEntityProcessingService {
         totalEnemysInWorld = enemiesInWorld;
         int rest = enemiesInWorld + deadEnemies;
         enemiesLeftToSpawn = targetEnemyCount - (enemiesInWorld + deadEnemies);
-    }
-
-    // Gets all info from the MapWave and updates the variables.
-    private void updateWaveInfo() {
-        currentWave = waves.get(currentWaveNumber - 1);
-        targetEnemyCount = currentWave.getEnemyAmount();
-        enemyType = currentWave.getEnemyType();
-
-    }
-
-    public MapSPI getMapSPI() {
-        return mapSPI;
-    }
-
-    public void setMapSPI(MapSPI mapSPI) {
-        this.mapSPI = mapSPI;
     }
 
     public void calculateCurrentRound(GameData gd) {
@@ -114,5 +70,41 @@ public class EnemySpawner implements IEntityProcessingService {
                 nextRoundTimer = 0f;
             }
         }
+    }
+
+    public int getEnemiesLeftToSpawn() {
+        return enemiesLeftToSpawn;
+    }
+
+    public float getLastSpawn() {
+        return lastSpawn;
+    }
+
+    public void incrementLastSpawn(float time) {
+        this.lastSpawn += time;
+    }
+
+    public float getSpawnTime() {
+        return spawnTime;
+    }
+
+    public String getEnemyType() {
+        return enemyType;
+    }
+
+    public MapWave getCurrentWave() {
+        return currentWave;
+    }
+
+    public ArrayList<MapWave> getWaves() {
+        return waves;
+    }
+
+    public void setWaves(ArrayList<MapWave> waves) {
+        this.waves = waves;
+    }
+
+    public void resetLastSpawn() {
+        this.lastSpawn = 0f;
     }
 }
